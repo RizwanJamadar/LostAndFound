f856ccf28cf8a44667987a9a5e208005dbefda95
Rizwan <ss19co055.rizwan.jamadar@gmail.com>
2023-06-05 10:27:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from './components/Navbar/Navbar';
+import Home from './pages/Home/Home'
+import Signup from './pages/Register/Register'
+import Login from './pages/Login/Login'
+import Product from './pages/Product/Product'
+import Upload from './pages/Upload/Upload'
+import UploadProduct from './pages/UploadProduct/UploadProduct';
+import Contact from './pages/Contact/Contact'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/signup' element={<><Signup /></>}></Route>
+          <Route path='/login' element={<><Login /></>}></Route>
+          <Route path='/product/:id' element={<Product/>}></Route>
+          <Route path='/upload' element={<><Upload /></>}></Route>
+          <Route path='/upload/details' element={<><UploadProduct /></>}></Route>
+          <Route path='/contact' element={<><Contact/></>}></Route>
+        </Routes>
+      </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
